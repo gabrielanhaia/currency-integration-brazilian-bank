4b39a1639abee492cce3819d48b9e80ddbf06e16
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2020-01-22 20:37:59 +0000

    feature: Implement parser layer

---
+++ Integration/Parser/TransactionParser.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace CurrencyFair\IntegrationBrazillianBank\Integration\Parser;
+
+
+use CurrencyFair\IntegrationBrazillianBank\Integration\Contract\IParser;
+
+/**
+ * Class TransactionParser
+ * @package CurrencyFair\IntegrationBrazillianBank\Integration\Parser
+ *
+ * @author Gabriel Anhaia <anhaia.gabriel@gmail.com>
+ */
+class TransactionParser implements IParser
+{
+    /**
+     * Parse raw data.
+     *
+     * @param mixed $rawData
+     * @return mixed
+     */
+    public function parse($rawData)
+    {
+        // TODO: Implement parse() method.
+    }
+}
