400a4efa86dcf1aaae06228b91bdb6317d673d3d
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2020-01-22 20:36:28 +0000

    feature: Implement formatter layer

---
+++ Integration/Formatter/TransactionFormatter.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace CurrencyFair\IntegrationBrazillianBank\Integration\Formatter;
+
+/**
+ * Class TransactionFormatter
+ * @package CurrencyFair\IntegrationBrazillianBank\Integration\Formatter
+ *
+ * @author Gabriel Anhaia <anhaia.gabriel@gmail.com>
+ */
+class TransactionFormatter
+{
+
+}
